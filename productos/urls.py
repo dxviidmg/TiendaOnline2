dc57e084a6fa2a371b1227b4d354ad58a9f5b8df
David <david@david-Lenovo-U310>
2016-10-09 16:15:19 -0500

    se arreglaron los titulos en los templates y en la lista de productos ya se ven horizontalmente

--- productos/urls.py
+++ productos/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	#url(r'^categoria/(?P<categoria_slug>[-\w]+)/$', views.ListProductos.as_view(), name="listProductoCategoria"),
+	url(r'^categoria/(?P<slug>[-\w]+)/$', views.ListProductos.as_view(), name="listProductoCategoria"),
 	url(r'^producto/(?P<slug>[-\w]+)/$', views.DetailProducto.as_view(), name='detailProducto'),
 	url(r'^productos', views.ListProductos.as_view(),name="listProductos"),
 ]
--- productos/views.py
+++ productos/views.py
@@ -4,14 +4,22 @@ from django.views.generic import View
 from carrito.forms import CartAddProductForm
 
 class ListProductos(View):
-	def get(self, request):
+	def get(self, request, slug=None):
 		template_name= "productos/listProductos.html"
+
+		if slug:
+			categoria = Categoria.objects.get(slug=slug)
+			productos = Producto.objects.all().filter(categoria=categoria)
+		else:
+			productos = Producto.objects.all()
+
 		categorias = Categoria.objects.all()
-		productos = Producto.objects.all()
+
 		form = CartAddProductForm()
 		context = {
 		'productos': productos,
 		'categorias': categorias,
+		
 		'form':  form,
 		}
 		return render(request, template_name, context)
